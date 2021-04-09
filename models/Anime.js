04487e5bfff9720dd86e46121061384bd0507415
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-04-09 14:02:04 -0300

    add Anime model

---
+++ models/Anime.js
@@ -0,0 +1,47 @@
+const { Schema, Types, model } = require('mongoose');
+
+const Anime = model('Anime', new Schema({
+    //Basic search info
+    externalId: { type: Number, required: true },
+    isAdult: Boolean,
+    //Identification
+    title: {
+        english: String,
+        romaji: String,
+        native: String
+    },
+    synonyms: [String],
+    coverImage: {
+        extraLarge: String,
+        large: String,
+        medium: String
+    },
+    bannerImage: String,
+    trailer: String,
+    relations: [Number],
+    //Descriptive info
+    description: String,
+    genres: [String],
+    tags: [String],
+    status: String,
+    //Format info
+    format: String,
+    episodes: Number,
+    duration: Number,
+    //Release info
+    status: String,
+    startDate: Date,
+    endDate: Date,
+    //Staff info
+    source: String,
+    originalCreator: String,
+    director: String,
+    mainStudio: String,
+    //Site related activity
+    favoriteOf: [{ type: Types.ObjectId, ref: User}],
+    beingWatchedBy: [{ type: Types.ObjectId, ref: User}],
+    watchedBy: [{ type: Types.ObjectId, ref: User}],
+    toBeWatchedBy: [{ type: Types.ObjectId, ref: User}]
+}))
+
+module.exports = Anime;
