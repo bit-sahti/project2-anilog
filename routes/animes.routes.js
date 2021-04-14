4fd1d0fa8d8c883366b50a1551a19999fb78841f
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-04-13 22:38:03 -0300

    finish add to lists logic

--- routes/animes.routes.js
+++ routes/animes.routes.js
@@ -7,6 +7,9 @@ router.get('/', async (req, res, next) => {
    try {
     const anime = await AniList.getAnime(req.query.id);
 
+    // let test = encodeURIComponent(JSON.stringify(anime))
+    // console.log(JSON.parse(decodeURIComponent(test)))
+
     res.render('./animes/anime', { anime: anime, currentUser: req.session.currentUser })
    }
 
@@ -16,26 +19,42 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/add', async (req, res, next) => {
-    const { _id: userId } = req.session.currentUser
-    const { id: animeId, list } = req.query
+    const { _id: userId } = req.session.currentUser;
+    const { id: animeId, userList, animeList } = req.query;
+    const currentUser = await User.findById(userId);
 
     console.log(req.query);
     
    try {
     let anime = await Anime.findOne({ externalId: animeId });
 
+    console.log('from db ========> ', anime ? anime._id : 'null')
+
     if (!anime) {
         animeFromAPI = await AniList.getAnime(animeId);
 
-        console.log(animeFromAPI)
+        console.log('from API =========>', animeFromAPI.externalId);
 
         anime = await Anime.create(animeFromAPI)
-
     }
-    console.log(anime.toBeWatchedBy)
+
+    if (!anime[animeList].includes(userId)) {
+        anime[animeList].push(userId)
+        
+        await anime.save()
+    }
+
+    if (!currentUser[userList].includes(anime._id)) {
+        currentUser[userList].push(anime._id);
+        await currentUser.save()
+    }    
+
+    console.log('operation completed ========>', currentUser[userList], anime._id, anime[animeList], currentUser._id)
+
+    res.redirect(`/anime/?id=${anime.externalId}`)
    }
    catch(err) {
-    console.log(err);
+    console.log('error ========> ', err);
 }
 })
 
