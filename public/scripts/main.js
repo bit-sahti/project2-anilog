ca8972c8413a512fa0901cfb5f9b4e5a35cb0202
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-04-19 21:19:37 -0300

    refactor code and add pagination funcionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,5 @@
+//Fomantic configuration
+
 $('.ui.dropdown')
   .dropdown()
 ;
@@ -6,59 +8,11 @@ $('#search-select')
   .dropdown()
 ;
 
+//DOM Manipulation
+const previous = document.getElementById('previous');
+const next = document.getElementById('next');
+const pageInput = document.getElementById('page')
 
-// const getAnimes = async event => {
-//     try {
-//         // if (event) event.preventDefault();
-    
-//         const titleInput = document.getElementById("title")
-        
-//         const genre_in = document.querySelectorAll('input[name="genre_in"]')
-//         const genreValues = []
+previous.onclick = () => pageInput.value = Number(pageInput.value) - 1;
 
-//         for (let i = 0; i < genre_in.length; i++) {
-//             if (genre_in[i].checked) genreValues.push(genre_in[i].value)
-//         }
-
-//         const { animes } = await aniList.searchMedia({
-//             [titleInput.name]: titleInput.value,
-//             genre_in: genreValues
-//         });
-//         console.log(animes);
-
-//         const resultsSection = document.getElementById("search-results")
-//         resultsSection.innerHTML = '';
-
-//         animes.forEach(anime => {
-//             document.getElementById("search-results").innerHTML += `
-//             <div class="anime-card">
-//                 <div>
-//                     <img src="${anime.coverImage.medium}" alt="">
-//                 </div>
-//                 <h3><a href="/anime?id=${anime.externalId}">${anime.title.english ? anime.title.english : anime.title.romaji }</a></h3>
-
-//             </div>
-//             `
-//         })
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// }
-
-// // getAnimes()
-// document.getElementById('go').addEventListener('click', getAnimes)
-// window.onhashchange = () => console.log('test');
-
-document.getElementById('next').onclick = event => {
-    // event.preventDefault();
-
-    let page = document.getElementById('page');
-    console.log(page.value);
-    page.value = Number(page.value) + 1;
-    console.log(page.value)
-
-    setTimeout(() => {
-      console.log('waiting');
-    }, 5000)
-}
+next.onclick = () => page.value = Number(pageInput.value) + 1;
