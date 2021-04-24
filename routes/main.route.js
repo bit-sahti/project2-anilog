83fe700a90927afef928482c4f4012e2462637de
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-04-24 10:32:28 -0300

    if there is no query, redirect index call to spring 2021 results page

--- routes/main.route.js
+++ routes/main.route.js
@@ -8,12 +8,14 @@ router.get('/', async (req, res, next) => {
     console.log('Get request on root completed');
     
     try {
+        if (!Object.keys(req.query).length) {
+            console.log('no query ====>', req.query);
 
-        console.log(req.query);
+            return res.redirect('/?seasonYear=2021&season=spring&sort=popularity_desc&perPage=30&page=1')
+        }
+
         const queryParams = queryMapper.map(req.query);
-        const media = await aniList.searchMedia(req.query)
-
-        console.log(media.pageInfo);
+        const media = await aniList.searchMedia(req.query);
 
         req.session.currentUser ? res.render('index', { currentUser: req.session.currentUser, query: queryParams, pageInfo: media.pageInfo, animes: media.animes }) 
                                 : res.render('index', { query: queryParams, pageInfo: media.pageInfo, animes: media.animes })
